23b5f347530d05e0bde38e7e1d64e6838c80db58
Yuuto <superhighlive@gmail.com>
2020-09-02 14:58:27 +0700

    them Go31-32

---
+++ Go31.py
@@ -0,0 +1,7 @@
+# Check Chan le
+def Check_ChanLe(value):
+    if value%2:
+        print("Day la mot so le!")
+    else:
+        print("Day la mot so chan!")
+Check_ChanLe(2)
---
+++ Go32.py
@@ -0,0 +1,9 @@
+#Lam viec voi Dict
+
+def Work_Dict():
+    d=dict()
+    d["Mot cai qq gi do"] = "aac"
+    d[2] = 2**2
+    d[3] = 3**3
+    print(d)
+Work_Dict()
